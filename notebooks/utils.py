203839af904721c55f143309af3052f7ea4e3ab0
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-26 12:01:47 +0545

    analysis as per rating added

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import json
 from tqdm import tqdm
 
+
 def write_to_file(data: Dict, file_path: Union[Path, str]) -> None:
     with open(file_path, "w") as f:
         f.writelines(json.dumps(data))
@@ -15,7 +16,7 @@ def read_json_file(path):
             data = json.loads(reader.read())
     except FileNotFoundError:
         print(f"{path} File Not Found")
-    except: 
+    except:
         print(f"Error Reading the FIle {path}")
 
     return data
@@ -30,10 +31,26 @@ def get_text_from_reviews(reviews):
     return texts
 
 
-def get_all_text(data):
-    reviews_text = []
+def get_all_text_data(data, rating=False):
+    result = []
+    process_func = None
+
+    if rating:
+        process_func = get_text_and_rating_from_reviews
+    else:
+        process_func = get_text_from_reviews
+
     for item in tqdm(data):
-        reviews_text += get_text_from_reviews(item["reviews"])
-    return reviews_text
+        result += process_func(item["reviews"])
+
+    return result
 
 
+def get_text_and_rating_from_reviews(reviews):
+
+    texts = []
+    for item in reviews["rows"]:
+        if item["text"]:
+            texts.append((item["text"], item["rating"]))
+
+    return texts

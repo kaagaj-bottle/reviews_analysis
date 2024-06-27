d5cb794f0a201b67f34af88ba008c44482d92fb6
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-27 17:30:10 +0545

    refined data for different thresholds added

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from pkg.utils import get_refined_data
 
 
 def main():
-    assert len(sys.argv) == 3
+    assert len(sys.argv) == 4
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
+    threshold = float(sys.argv[3])
 
     get_refined_data(
         raw_reviews_path=input_file,
@@ -16,7 +17,7 @@ def main():
         model="en_core_web_md",
         top_n_word=95,
         top_n_association=5,
-        threshold=0.8
+        threshold=threshold
     )
 
 

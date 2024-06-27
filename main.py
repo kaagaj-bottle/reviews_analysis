47160417261d4510b991a0a44c15e24f23d257f1
Zishan Siddique <zishansiddique80@gmail.com>
2024-06-27 21:43:27 +0545

    added refined_data for different models

--- main.py
+++ main.py
@@ -4,20 +4,21 @@ from pkg.utils import get_refined_data
 
 
 def main():
-    assert len(sys.argv) == 4
+    assert len(sys.argv) == 5
 
     input_file = sys.argv[1]
     output_file = sys.argv[2]
-    threshold = float(sys.argv[3])
+    model = sys.argv[3]
+    threshold = float(sys.argv[4])
 
     get_refined_data(
         raw_reviews_path=input_file,
         output_file_path=output_file,
-        rating_values=["good","bad"],
-        model="en_core_web_lg",
+        rating_values=["good", "bad"],
+        model=model,
         top_n_word=95,
         top_n_association=5,
-        threshold=threshold
+        threshold=threshold,
     )
 
 
